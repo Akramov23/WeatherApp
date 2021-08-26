17582a98b10acb8c7f61a0dc8e51b71e9e8c23b4
Neo <muhammadbobur8522@gmail.com>
2021-08-26 22:15:44 +0500

    first commit

---
+++ src/WeatherApp/TopNav.js
@@ -0,0 +1,10 @@
+function TopNav(){
+    return(
+        <>
+           <div className="topNav">
+               <h3>Akramov | Weather App</h3>   
+            </div>  
+        </>
+    )
+}
+export default TopNav
